f7c2706be6a1915c088f858d5c9e848da2a83238
Sebastián Salata R-T <sa.salatart@gmail.com>
2020-02-23 11:23:05 -0300

    feat: add parser to create table

--- src/index.js
+++ src/index.js
@@ -2,11 +2,34 @@
 function onOpen() {
   SpreadsheetApp.getUi()
     .createMenu("SII")
-    .addItem("Hello World", "helloWorld")
+    .addItem("Transformar", "parse")
     .addToUi();
 }
 
 // eslint-disable-next-line no-unused-vars
-function helloWorld() {
-  console.log("Hello world!");
+function parse() {
+  let valuesGenerator;
+  try {
+    const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("in");
+    if (!sheet) throw new NoInputSheetFoundError();
+
+    const displayValues = sheet.getDataRange().getDisplayValues();
+    valuesGenerator = makeValuesGenerator(displayValues);
+  } catch (err) {
+    handleError(err);
+    return;
+  }
+
+  const finalValues = [UPPER_HEADERS.concat(LOWER_HEADERS)];
+  let nextValue = valuesGenerator.next();
+  while (!nextValue.done) {
+    const { upperRow, lowerRows } = nextValue.value;
+    lowerRows.forEach(lowerRow => {
+      finalValues.push(upperRow.concat(lowerRow));
+    });
+    nextValue = valuesGenerator.next();
+  }
+
+  // eslint-disable-next-line no-console
+  console.log(finalValues);
 }
